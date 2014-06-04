8d896ec02ff295c3d4be33316cbd845c331a162e
Anthony Morel <anthony@Alordis.co.uk>
2014-06-04 17:23:44 +0100

    Removed add_options(), making command line options generic across transcoders Complete the implementation of flac transcoder, expect for multichannel sacd transcoder is in progress (so broken)

--- flac.py
+++ flac.py
@@ -3,11 +3,22 @@ import flacmeta
 import metautils
 import subprocess
 import logging
+    
+def add_dsp_downsampler(dsp, srate):
+    # Apply a short filter for higher sampling rate
+    # '95' at 48kHz gives passband of 0 .. 23 kHz
+    # '74' at 96kHz gives passband of 0 .. 35 kHz and less utrasonic echoes
+    dsp += ['rate','-b','95' if srate <= 48000 else '74',str(srate)]
 
-def add_options(parser):
-    parser.add_argument('-s', choices=('48k','96k'), default='192k', help='downsample to this maximum sample rate as required')
+def add_dsp_gain(dsp, gain):
+    dsp += ['gain', str(gain)]
 
-def reencode_same_rate(flacfile, outfile):
+def reencode_with_dsp(flacfile, outfile, dsp):
+    cmd = ['sox','-G',flacfile,'-C','8',outfile] + dsp
+    logging.debug(cmd)
+    subprocess.call(cmd)
+
+def reencode_no_dsp(flacfile, outfile):
     # flac -8 -s <input.flac> -o <output.flac> --> may fail with ERROR: input file has an ID3v2 tag
     # Use flac -c -d <input.flac> | flac -8 -s - -o <output.flac>
     cmd = ['flac','-c','-s','-d',flacfile]
@@ -17,23 +28,6 @@ def reencode_same_rate(flacfile, outfile):
     p2 = subprocess.Popen(cmd, stdin=p.stdout)
     logging.debug(cmd)
     p2.communicate()
-
-def reencode_96k(flacfile, outfile):
-    # Don't need wasteful ultrasonics (noise / DSD noise shaping, spurs)
-    # Keep bandwith = 0 .. 35 kHz using a short filter to prevent
-    # (ultrasonic) echoes
-    cmd = ['sox','-G',flacfile,'-C','8',outfile,'rate','-b','74','96000']
-    logging.debug(cmd)
-    subprocess.call(cmd)
-
-def reencode_48k(flacfile, outfile):
-    # Don't need wasteful ultrasonics (noise / DSD noise shaping, spurs)
-    # Keep bandwith = 0 .. 23 kHz
-    cmd = ['sox','-G',flacfile,'-C','8',outfile,'rate','-b','95','48000']
-    logging.debug(cmd)
-    subprocess.call(cmd)
-
-reencode_algo = { '48k':reencode_48k, '96k':reencode_96k, '192k':reencode_same_rate }
 
 class transcoder:
     def __init__(self, args=None):
@@ -68,10 +62,17 @@ class transcoder:
         logging.info('Processing\t'+f)
         pathname = os.path.join(self.directory,f)
         outfile = metautils.get_filename(outdir,self.filemeta[f])
-        if self.filemeta[f]['srate'] > int(self.args.s[:-1])*1000:
-            reencode_algo[self.args.s](pathname, outfile)
+        dsp = []
+        if self.filemeta[f]['channels'] > 2 and self.args.m == True:
+            raise NotImplementedError("Downmix of multi-channel flac is not implemented yet.")
+        if self.filemeta[f]['srate'] > self.args.s:
+            add_dsp_downsampler(dsp, self.args.s)
+        if self.args.g != 0:
+            add_dsp_gain(dsp, self.args.g)
+        if dsp:
+            reencode_with_dsp(pathname, outfile, dsp)
         else:
-            reencode_same_rate(pathname, outfile)
+            reencode_no_dsp(pathname, outfile)
         metadata = { k:self.filemeta[f][k] for k in
             ('album','artist','date','tracknumber','title') if k in self.filemeta[f] }
         logging.debug(metadata)
@@ -97,17 +98,44 @@ class transcoder:
 
 
 if __name__ == '__main__':
-    import argparse
-    from pprint import pprint
-    logging.basicConfig(level=logging.DEBUG)
-    parser = argparse.ArgumentParser(prog='PROG')
-    parser.add_argument('rootdir')
-    add_options(parser)
-    t=transcoder(parser.parse_args(['-s','48k','.']))
-    pprint(vars(t))
-    #t.args=parser.parse_args([])
-    pprint(t.probe('/home/amorel/Music/Seawind - (1980) Seawind'))
-    t.srate=176400
-    print(t.transcode())
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Test 1')
+    class args:
+        s = 48000
+        rootdir = '.'
+        m = False
+        g = 0
+    t=transcoder(args)
+    f=t.probe('testset/cd')
+    assert f, 'check testset/cd folder for cd quality flac files'
+    d=t.transcode()
+    assert d, 'transcode did not create output folder'
+    logging.warn('DONE - Test 1. Verify '+str(d)+' matches testset/cd')
 
+    logging.info('Test 2')
+    f=t.probe('testset/hr')
+    assert f, 'check testset/hr folder for high-res flac files'
+    d=t.transcode()
+    assert d, 'transcode did not create output folder'
+    logging.warn('DONE - Test 2. Verify '+str(d)+' is testset/hr downsampled to 48kHz')
 
+    logging.info('Test 3')
+    f=t.probe('testset/mixed')
+    assert f, 'check testset/mixed for two+ different album files in same folder'
+    d=t.transcode()
+    assert len(d) >= 2, 'transcode did not create at least 2 output folders'
+    logging.warn('DONE - Test 3. Verify '+str(d)+' span all tracks in testset/mixed')
+
+    logging.info('Test 4')
+    class args:
+        s = 48000
+        rootdir = '.'
+        m = False
+        g = -6
+    t=transcoder(args)
+    f=t.probe('testset/cd')
+    assert f, 'check testset/cd folder for cd quality flac files'
+    d=t.transcode()
+    logging.warn('DONE - Test 4. Verify '+str(d)+' is testset/cd attenuated by 6 dB')
+
+    # TODO: test case of different albums in same folder
--- sacd.py
+++ sacd.py
@@ -1,29 +1,96 @@
-def int_or_off(s):
-    if s.lower()=='off':
-        return None
-    else:
-        return int(s)
+import os
+import subprocess
+import logging
 
-def add_options(parser):
-    parser.add_argument('-m', action='store_true',
-        help='(sacd) mix multichannel down to stereo')
-    parser.add_argument('--front', type=int_or_off, metavar='{G,off}', default=0,
-        help='front channels gain in dB or off')
-    parser.add_argument('--ctr', type=int_or_off, metavar='{G,off}', default=0,
-        help='centre channel gain in dB or off')
-    parser.add_argument('--sub', type=int_or_off, metavar='{G,off}', default=0,
-        help='subwoofer channel gain in dB or off')
-    parser.add_argument('--rear', type=int_or_off, metavar='{G,off}', default=0,
-        help='rear channels gain in dB or off')
+def sacd_extract(isofile):
+    cmd = ['sacd_extract','-i', isofile, '-P', '-2', '-p', '-c']
+    output = subprocess.check_output(cmd)
+    return output
 
 class transcoder:
     def __init__(self, args=None):
         self.args = args
         self.directory = ''
+        self.dff = []       # Intermediary DFF files
+        self.metadata = {}  # Common metadata (no track titles and numbers)
+        self.titles = []    # Track titles (implicit numbering)
 
     def probe(self, directory):
-        return []
+        self.directory = directory
+        self.files=sorted([name for name in os.listdir(directory) if name.lower().endswith('.iso')])
+        return self.files
+
+    def _extract_metadata(self, log):
+        self.metadata = {}
+        album, artist, date = metautils.infer_from_dir(self.directory)
+        match = re.findall( r'Title:\s*(.*)', log, re.MULTILINE)
+        if match:
+            self.metadata['album'] = match[0]
+        else:
+            self.metadata['album'] = album
+        match = re.findall( r'Artist:\s*(.*)', data, re.MULTILINE)
+        if match:
+            self.metadata['artist'] = match[0]
+        else:
+            self.metadata['artist'] = artist
+        match = re.findall( r'19\d{2}|20\d{2}', data, re.MULTILINE)
+        if match:
+            match.sort()
+            self.metadata['date'] = match[0]
+        if date:
+            if ('date' not in self.metadata) or (date < metadata['date']):
+                self.metadata['date'] = date
+        self.titles = re.findall( r'Title\[\d+\]:\s*(.*)', data, re.MULTILINE)
+        self.dff = re.findall( r'Processing \[(.*)\]', data, re.MULTILINE)
+
+    def _transcode_one(self, f, outdir):
+        logging.info('Processing\t'+f)
+        pathname = os.path.join(self.directory,f)
+        outfile = metautils.get_filename(outdir,self.metadata)
+        # dff2raw -i <file.dff> | sox -t raw -e float -b 32 -r 2822400 -c 2 - -b 24 <file.flac> rate -v 48000 gain 6 stats
+        cmd = ['dff2raw','-i',f]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        logging.debug(cmd)
+        cmd = ['sox','-t','raw','-e','float','-b','32','-r','2822400','-c','2','-',
+            '-b','24',outfile,'rate','-v','48k','gain','6']
+        p2 = subprocess.Popen(cmd, stdin=p.stdout)
+        logging.debug(cmd)
+        p2.communicate()
+        logging.debug(self.metadata)
+        flacmeta.set_meta(self.metadata, outfile)
 
     def transcode(self):
-        return []
+        self.outdir = []
+        for f in self.files:
+            # Convert to DFF using sacd_extract() and parse info from log
+            log = sacd_extract(f)
+            self._extract_metadata(log)
+            if self.dff:
+                outdir = metautils.get_output_dir(self.args.rootdir, self.metadata)
+                os.mkdir(outdir)
+                self.outdir.append(outdir)
+                for idx, dff in enumerate(self.dff):
+                    self.metadata['tracknumber'] = '%02d' % (idx+1)
+                    self.metadata['title'] = self.titles[idx]
+                    self._transcode_one(dff, outdir)
+                    # remove intermediary file created by sacd_extract
+                    os.remove(dff)
+                # remove intermediary directory created by sacd_extract
+                os.rmdir(os.path.dirname(files[0]))
+        return self.outdir
+
+if __name__ == '__main__':
+    import argparse
+    from pprint import pprint
+    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser(prog='PROG')
+    parser.add_argument('rootdir')
+    add_options(parser)
+    t=transcoder(parser.parse_args(['-s','48k','-g','6','.']))
+    pprint(vars(t))
+    #t.args=parser.parse_args([])
+    # TODO:
+    pprint(t.probe('/path/to/sacd.iso'))
+    print(t.transcode())
+
 
