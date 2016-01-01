35f1eaed5e5ffb1d212e3985ca57ceb152e84dc1
Anthony Morel <anthony.morel@bsintl.com>
2016-01-01 16:51:10 +0000

    Refactor SACD, splitting out metadata collection
    Also get metadata corresponding to the requested area (stereo, mch) as
    the number of tracks and their names may differ btw areas.
    
    We do a separate pass of sacd_extract (which is almost instant) to
    extract the metadata, but removed parsing dff files and the dependency
    to self.dff.

--- sacd.py
+++ sacd.py
@@ -5,26 +5,18 @@ import re
 import tempfile
 from metautils import *
 
-def get_dff_params(f):
-    cmd = ['dff2raw', '-p', f]
-    output = subprocess.check_output(cmd)
-    match = re.search(r'^numChannels\s*=\s*(\d+)', output, re.MULTILINE)
-    if match:
-        channels = int(match.group(1))
-    match = re.search(r'^sampleRate\s*=\s*(\d+)', output, re.MULTILINE)
-    if match:
-        srate = int(match.group(1))
-    logging.debug(str((channels, srate)))
-    return channels, srate
-
 class sacdtranscoder:
 
     def __init__(self, args={}):
         self.args = args
         # self.directory    -> input directory
-        # self.dff          -> Intermediary DFF files
         # self.metadata     -> Common metadata (no track titles and numbers)
         # self.titles       -> Track titles (implicit numbering)
+        # self.channels     -> Number of channels of current iso>area (2,5,6)
+
+    def _mch(self):
+        logging.debug(self.args)
+        return self.args['mix'] or self.args['mch']
 
     def probe(self, directory):
         self.directory = directory
@@ -33,12 +25,9 @@ class sacdtranscoder:
                 if name.lower().endswith('.iso')])
         self.files = []
         for isofile in files:
-            cmd = ['sacd_extract', '-i', os.path.realpath(isofile), '-P']
-            logging.debug(cmd)
-            output = subprocess.check_output(cmd)
-            logging.debug(output)
+            output = self._sacd_extract_info(isofile)
             match = re.findall(r'Speaker config: (\d) Channel', output, re.MULTILINE)
-            if self.args['mix'] or self.args['mch']:
+            if self._mch():
                 # Require a multichannel track
                 if not ('5' in match or '6' in match):
                     logging.info(isofile + ' has no multichannel track')
@@ -50,7 +39,8 @@ class sacdtranscoder:
             self.files.append(isofile)
         return self.files
 
-    def _extract_metadata(self, log):
+    def _extract_metadata(self, isofile):
+        log = self._sacd_extract_info(isofile)
         self.metadata = {}
         album, artist, date = infer_from_dir(self.directory)
         match = re.findall(r'Title:\s*(.*)', log, re.MULTILINE)
@@ -71,22 +61,33 @@ class sacdtranscoder:
         if date:
             if ('date' not in self.metadata) or (date < self.metadata['date']):
                 self.metadata['date'] = date
-        logging.debug('date ' + self.metadata['date'])
-        self.titles = re.findall(r'Title\[\d+\]:\s*(.*)', log, re.MULTILINE)
+            logging.debug('date ' + self.metadata['date'])
+        # Get metadata corresponding to the requested area (stereo, mch)
+        # as the number of tracks and their names may differ btw areas
+        area = re.split(r'Speaker config: ', log, 0, re.MULTILINE)
+        area = area[2] if self._mch() else area[1]
+        logging.debug(area)
+        self.channels = int(area[0])
+        self.titles = re.findall(r'Title\[\d+\]:\s*(.*)', area, re.MULTILINE)
         self.titles = dontshout('\n'.join(self.titles)).split('\n')
         logging.debug(self.titles)
-        self.dff = re.findall(r'Processing \[(.*)\]', log, re.MULTILINE)
-        logging.debug(self.dff)
+
+    def _sacd_extract_info(self, isofile):
+        # Extracts sacd area info
+        cmd = ['sacd_extract', '-i', os.path.realpath(isofile), '-P']
+        logging.debug(cmd)
+        output = subprocess.check_output(cmd)
+        logging.debug(output)
+        return output
 
     def _sacd_extract(self, isofile):
         # Extracts the sacd tracks into dff files
         # This can take many minutes due to dst de-compression
         tmpdir = tempfile.mkdtemp()
-        cmd = ['sacd_extract', '-i', os.path.realpath(isofile),
-               '-P', '-p', '-c']
+        cmd = ['sacd_extract', '-i', os.path.realpath(isofile), '-p', '-c']
         if self.args['t']:
             cmd += ['-t',','.join(self.args['t'])]
-        if self.args['mix'] or self.args['mch']:
+        if self._mch():
             cmd += ['-m']
         logging.debug(cmd)
         output = subprocess.check_output(cmd, cwd=tmpdir)
@@ -96,18 +97,19 @@ class sacdtranscoder:
     def _transcode_one(self, f, outdir):
         outfile = get_filename(outdir, self.metadata)
         logging.info('Creating\t' + os.path.basename(outfile))
-        channels, srate = get_dff_params(f)
         # dff2raw <file.dff> | sox -t raw -e float -b 32 -r 2822400 -c 2 -
         # -b 24 <file.flac> rate -v 48000 gain 6 stats
         cmd = ['dff2raw', f]
-        if self.args['mix'] and (channels == 5 or channels == 6):
+        if self.args['mix'] and self.channels >= 5:
             channels = 2
             cmd += ['-m'] + \
                    ['-'+k[0]+str(self.args[k])
                     for k in ('front', 'ctr', 'rear', 'sub')]
+        else:
+            channels = self.channels
         logging.debug(cmd)
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        cmd = ['sox', '-t', 'raw', '-e', 'float', '-b', '32', '-r', str(srate),
+        cmd = ['sox', '-t', 'raw', '-e', 'float', '-b', '32', '-r', '2822400',
                '-c', str(channels), '-', '-b', '24', outfile,
                'rate', '-v', str(self.args['srate']), 'gain', str(self.args['gain'])]
         logging.debug(cmd)
@@ -119,16 +121,19 @@ class sacdtranscoder:
     def transcode(self):
         self.outdir = []
         for f in self.files:
+            isofile = os.path.join(self.directory, f)
+            self._extract_metadata(isofile)
             # Convert to DFF using sacd_extract() and parse info from log
-            tmpdir, log = self._sacd_extract(os.path.join(self.directory, f))
-            self._extract_metadata(log)
-            if self.dff:
+            tmpdir, log = self._sacd_extract(isofile)
+            dffs = re.findall(r'Processing \[(.*)\]', log, re.MULTILINE)
+            logging.debug(dffs)
+            if dffs:
                 outdir = get_output_dir(
                     self.args['rootdir'], self.metadata)
                 os.mkdir(outdir)
                 logging.info('To ' + outdir)
                 self.outdir.append(outdir)
-                for idx, f in enumerate(self.dff):
+                for idx, f in enumerate(dffs):
                     logging.debug('Processing\t' + f)
                     dff = os.path.join(tmpdir, f)
                     self.metadata['tracknumber'] = '%02d' % (idx + 1)
